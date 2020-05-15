84e1e0ba36f05ad6941816db95bc24f74659ee88
adityasunny1189 <adityapathak1189@gmail.com>
2020-05-16 01:24:13 +0530

    logo added

---
+++ build/precache-manifest.7d50e81978612a8c4e6efe17227c85a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47d48b82471e0f143b2f63e0f175b153",
+    "url": "/Meme-World/index.html"
+  },
+  {
+    "revision": "98aa1baf1316f7e71141",
+    "url": "/Meme-World/static/css/main.0049469d.chunk.css"
+  },
+  {
+    "revision": "c9dfdfbddda5ffde30c9",
+    "url": "/Meme-World/static/js/2.0728adcb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Meme-World/static/js/2.0728adcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98aa1baf1316f7e71141",
+    "url": "/Meme-World/static/js/main.6a573dc3.chunk.js"
+  },
+  {
+    "revision": "3b025ca0c1441677630e",
+    "url": "/Meme-World/static/js/runtime-main.79635066.js"
+  },
+  {
+    "revision": "531f932d90a5fc2c8bcc678d9beeefb8",
+    "url": "/Meme-World/static/media/mememaker.531f932d.png"
+  }
+]);
--- src/Components/header.components.jsx
+++ src/Components/header.components.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './header.css'
+import meme from './mememaker.png'
 
 const Header = () => {
     return (
         <div className = "header">
             <img 
                 className = "logo"
-                src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
+                src={meme} 
                 alt="Problem?"
             />
             <img className = "main-image" alt = "meme-generator" src = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSAhTaWUyZKb2dUDDFRENJzbeehWZrA4GutYOtK3o4Vdaj-uGEW&usqp=CAU" />
